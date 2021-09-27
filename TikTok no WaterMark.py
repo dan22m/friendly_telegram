3ed5b614ba68505fddd7e6069df7b53674fd154b
dan22m <daniel666maklein@gmail.com>
2021-09-27 14:54:56 +0300

    1

---
+++ TikTok no WaterMark.py
@@ -0,0 +1,20 @@
+﻿from .. import loader, utils
+
+
+def register(cb):
+    cb(TikTokMod())
+
+class TikTokMod(loader.Module):
+    """Завантажуємо відео без їбаної реклами тт"""
+    strings = {'name': 'TikTok no WaterMark'}
+
+    async def tikcmd(self, message):
+        """Кидає тобі в їбало відео без їбаної реклами тт"""
+        await utils.answer(message, 'Та зачекай...')
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, "А лінк я звідки візьму? А? Забув?")
+            return
+        r = await message.client.inline_query('tikdobot', args)
+        await message.client.send_file(message.to_id, r[1].result.content.url)
+        await message.delete()
